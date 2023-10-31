9ec7dc8ccfdd19d73f763a64879db0e6cfe0ae1f
cheorhyeon <r4560798@naver.com>
2023-10-31 19:43:47 +0900

    setting : config 파일 생성

---
+++ src/main/java/com/wanted/restaurant/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.wanted.restaurant.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(title = "restaurant",
+                description = "teemO 2nd Project restaurant",
+                version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+}
