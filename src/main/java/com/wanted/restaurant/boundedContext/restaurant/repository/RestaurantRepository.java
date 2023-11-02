a696f0ad9e7b2ff33880e30d6a960a6cc515e4f8
johan1103 <johan1103@naver.com>
2023-11-03 01:26:22 +0900

    feat: native query for ST_DISTANCE procedure #3

--- src/main/java/com/wanted/restaurant/boundedContext/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/wanted/restaurant/boundedContext/restaurant/repository/RestaurantRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
-  @Query(value = "select r from Restaurant r where ST_Distance_Sphere(POINT(r.lat,r.lng),POINT(:lat,:lng))<=:maxDistance " +
+  @Query(value = "select r from Restaurant r where ST_Distance_Sphere(POINT(r.lat,r.lng),POINT(:lat, :lng))<=:maxDistance " +
           "order by ST_Distance_Sphere(POINT(r.lat,r.lng),POINT(lat,lng)) desc",nativeQuery = true)
   Slice<Restaurant> searchRestaurants(@Param(value = "lat")double lat, @Param(value = "lng")double lng,
                                       @Param(value = "maxDistance")double maxDistance, Pageable pageable);
